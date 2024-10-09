c534e92b8bd91ce85d403a956caac42b1ae79336
Oskar Sollenberg <122973984+OskarSollenberg@users.noreply.github.com>
2024-10-09 09:00:27 +0200

    feat: first commit

---
+++ schemaTypes/eventType.ts
@@ -0,0 +1,71 @@
+import {defineField, defineType} from 'sanity'
+
+export const eventType = defineType({
+  name: 'event',
+  title: 'Event',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+      },
+      validation: (Rule) => Rule.required().error('Slug is required'),
+      hidden: ({document}) => !document?.name,
+    }),
+    defineField({
+      name: 'eventType',
+      type: 'string',
+      options: {
+        list: ['in-person', 'virtual'],
+        layout: 'radio',
+      },
+    }),
+    defineField({
+      name: 'date',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'doorsOpen',
+      description: 'Number of minutes before the start time for admission',
+      type: 'number',
+      initialValue: 60,
+    }),
+    defineField({
+      name: 'venue',
+      type: 'reference',
+      to: [{type: 'venue'}],
+      validation: (Rule) =>
+        Rule.custom((value, context) => {
+          if (value && context?.document?.eventType === 'virtual') {
+            return 'Only in-person events can have a venue'
+          }
+          0
+          return true
+        }),
+    }),
+    defineField({
+      name: 'headline',
+      type: 'reference',
+      to: [{type: 'artist'}],
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'details',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+      name: 'tickets',
+      type: 'url',
+    }),
+  ],
+})
